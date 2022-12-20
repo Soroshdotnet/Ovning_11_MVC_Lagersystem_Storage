092f7a93bfc95b1286c2c80c93064b84892e52d0
soroshdotnet <sorosh.farahmand@gmail.com>
2022-12-20 21:46:42 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ovning_11_MVC_Lagersystem_Storage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
